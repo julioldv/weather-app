e7b009cd677d33d37327f397b90ca53fed6c543e
Julio Cesar <172700125+julioldv@users.noreply.github.com>
2026-06-18 17:16:54 -0600

    Allow fetching weather by location

--- src/index.js
+++ src/index.js
@@ -1,2 +1,35 @@
 import "modern-normalize";
 import "./styles.css";
+
+async function getWeatherData(location) {
+  try {
+    const safeLocation = encodeURIComponent(location);
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${safeLocation}?unitGroup=us&key=UM7FR7HW2RBCY3SJRXKCVMERM&contentType=json`,
+    );
+    if (!response.ok) throw new Error(`HTTP error: ${response.status}`);
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+function processWeatherData(data) {
+  return {
+    location: data.resolvedAddress,
+    temperature: data.days[0].temp,
+    conditions: data.days[0].conditions,
+  };
+}
+
+async function displayTestWeatherData() {
+  const rawWeatherData = await getWeatherData("Mexico City");
+  const processedWeatherData = processWeatherData(rawWeatherData);
+
+  console.log(processedWeatherData);
+}
+
+displayTestWeatherData();
